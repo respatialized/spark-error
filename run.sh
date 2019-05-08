b30b0a10a19477a61fb74a32f5b8e408648ce364
Andrew Foltz-Morrison <andrew.foltz-morrison@sitomobile.com>
2019-05-08 11:50:09 -0400

    initial commit

---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+if [ "$1" == "-h" ]; then
+    echo "Usage: `basename $0` [conda-dir conda-env sbt-cmd]"
+    echo "conda-dir: the path of your anaconda installation (should end in ../conda.sh)"
+    echo "conda-env: the path of the conda env with Databricks Connect installation"
+    echo "sbt-cmd: the sbt command to execute"
+    exit 0
+fi
+
+# configuring the connection environment
+printf "making Conda path available\n"
+source $1
+printf "activating conda env and configuring connection\n"
+conda activate $2
+databricks-connect configure
+
+# create jar location on classpath (if it doesn't exist)
+if [ -d "./lib" ]; then
+   printf "unmanaged jar directory exists already\n"
+else
+    printf "creating unmanaged jar directory on classpath\n"
+    mkdir lib
+fi
+
+# copy jars to env (if not already done)
+if [ -z $(diff $(databricks-connect get-jar-dir)/*.jar ./lib)]; then
+    printf "databricks connect jars already on classpath\n"
+else
+    printf "copying and overwriting jars to classpath from dbconnect path\n"
+    cp -f $(databricks-connect get-jar-dir)/*.jar ./lib
+fi
+
+printf "configuration complete.\n\n"
+
+# executing job
+printf "executing spark job"
+sbt $3
